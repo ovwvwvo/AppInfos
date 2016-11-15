9e5c394f47d6a0e8a72f8271523624ef837f7039
ovwvwvo <jiayaguang123@gmail.com>
2016-11-15 18:43:05 +0800

    添加设置页面

--- app/src/main/java/com/ovwvwvo/appinfos/ui/SettingActivity.java
+++ app/src/main/java/com/ovwvwvo/appinfos/ui/SettingActivity.java
@@ -2,15 +2,32 @@ package com.ovwvwvo.appinfos.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+
+import com.ovwvwvo.appinfos.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Copyright ©2016 by ovwvwvo
  */
 
-public class SettingActivity extends BaseActivity{
+public class SettingActivity extends BaseActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
+        initToolBar();
+    }
 
+    private void initToolBar() {
+        setSupportActionBar(toolbar);
+        getActionBar().setDisplayShowTitleEnabled(true);
+        getActionBar().setTitle(getString(R.string.settings));
     }
 }
