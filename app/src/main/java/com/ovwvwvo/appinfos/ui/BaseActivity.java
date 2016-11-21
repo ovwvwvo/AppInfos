b7435e2fa4caff4be603cfe48239a60b75440d6d
ovwvwvo <jiayaguang123@gmail.com>
2016-11-21 22:36:59 +0800

    添加Umeng 统计

--- app/src/main/java/com/ovwvwvo/appinfos/ui/BaseActivity.java
+++ app/src/main/java/com/ovwvwvo/appinfos/ui/BaseActivity.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.appcompat.BuildConfig;
+
+import com.umeng.analytics.MobclickAgent;
 
 /**
  * Copyright ©2016 by ovwvwvo
@@ -17,4 +20,17 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mContext = this;
     }
+
+    public void onResume() {
+        super.onResume();
+        if (!BuildConfig.DEBUG)
+            MobclickAgent.onResume(mContext);
+    }
+
+    public void onPause() {
+        super.onPause();
+        if (!BuildConfig.DEBUG)
+            MobclickAgent.onPause(mContext);
+    }
+
 }
