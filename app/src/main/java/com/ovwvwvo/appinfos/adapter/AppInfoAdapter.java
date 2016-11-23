8222fad55dd4849511566cbf8a512e0c018aab6f
ovwvwvo <jiayaguang123@gmail.com>
2016-11-23 18:32:07 +0800

    替换 toast 为 Snackbar

--- app/src/main/java/com/ovwvwvo/appinfos/adapter/AppInfoAdapter.java
+++ app/src/main/java/com/ovwvwvo/appinfos/adapter/AppInfoAdapter.java
@@ -3,13 +3,13 @@ package com.ovwvwvo.appinfos.adapter;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ovwvwvo.appinfos.R;
 import com.ovwvwvo.appinfos.model.AppInfoModel;
@@ -80,7 +80,14 @@ public class AppInfoAdapter extends RecyclerView.Adapter<AppInfoAdapter.AppInfoV
             ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
             ClipData clip = ClipData.newPlainText("appInfo", models.get(getAdapterPosition()).toString());
             clipboard.setPrimaryClip(clip);
-            Toast.makeText(context, "信息已复制", Toast.LENGTH_SHORT).show();
+            Snackbar.make(icon, R.string.copy, Snackbar.LENGTH_LONG)
+                .setAction("分享", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //// TODO: 2016/11/23 shape
+                    }
+                })
+                .show();
         }
     }
 }
