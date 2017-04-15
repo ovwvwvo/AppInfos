5e35cbff42e86d19c3627df0cad97e1115f322c0
ovwvwvo <jiayaguang123@gmail.com>
2017-04-15 23:07:00 +0800

    add AppinfoDetail UI

--- app/src/main/java/com/ovwvwvo/appinfos/ui/AppDetailActivity.java
+++ app/src/main/java/com/ovwvwvo/appinfos/ui/AppDetailActivity.java
@@ -4,7 +4,8 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.ImageView;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.ovwvwvo.appinfos.R;
@@ -12,22 +13,29 @@ import com.ovwvwvo.appinfos.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Copyright ©2017 by Teambition
  */
 
-public class AppDetailActivity extends BaseActivity {
+public class AppDetailActivity extends BaseActivity implements AppBarLayout.OnOffsetChangedListener {
+
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f;
 
     public static final String PACKAGE_NAME = "package_name";
     private String packageName;
 
-    @BindView(R.id.icon)
-    ImageView icon;
+    @BindView(R.id.appBar)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.app_icon)
+    CircleImageView appIcon;
     @BindView(R.id.app_name)
     TextView appName;
-    @BindView(R.id.version)
-    TextView version;
+    @BindView(R.id.app_version)
+    TextView appVersion;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +43,8 @@ public class AppDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
         packageName = getIntent().getStringExtra(PACKAGE_NAME);
+        appBarLayout.addOnOffsetChangedListener(this);
+
         initData();
     }
 
@@ -42,10 +52,10 @@ public class AppDetailActivity extends BaseActivity {
         PackageManager pm = getPackageManager();
         try {
             PackageInfo packageInfo = pm.getPackageInfo(packageName, PackageManager.GET_META_DATA);
-            icon.setImageDrawable(pm.getApplicationIcon(packageName));
+            appIcon.setImageDrawable(pm.getApplicationIcon(packageName));
             appName.setText(packageInfo.applicationInfo.loadLabel(pm).toString());
             String str = String.format(getString(R.string.detail_version), packageInfo.versionName);
-            version.setText(str);
+            appVersion.setText(str);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
@@ -92,5 +102,18 @@ public class AppDetailActivity extends BaseActivity {
 
     }
 
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(verticalOffset) / (float) maxScroll;
+        handleToobarDisplay(percentage);
+    }
 
+    private void handleToobarDisplay(float percentage) {
+        if (percentage > PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+            toolbar.setTitle(appName.getText());
+        } else {
+            toolbar.setTitle("");
+        }
+    }
 }
