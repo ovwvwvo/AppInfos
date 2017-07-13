a62761423a6e224b60e1283536a9dff566917acc
rawer <rawer.dev@gmail.com>
2017-07-13 13:17:25 +0800

    remove umeng analytics

--- app/src/main/java/com/ovwvwvo/appinfos/ui/BaseActivity.java
+++ app/src/main/java/com/ovwvwvo/appinfos/ui/BaseActivity.java
@@ -3,9 +3,6 @@ package com.ovwvwvo.appinfos.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.appcompat.BuildConfig;
-
-import com.umeng.analytics.MobclickAgent;
 
 /**
  * Copyright ©2016 by rawer
@@ -19,17 +16,4 @@ public class BaseActivity extends com.ovwvwvo.common.activity.BaseActivity {
         super.onCreate(savedInstanceState);
         this.mContext=this;
     }
-
-    public void onResume() {
-        super.onResume();
-        if (!BuildConfig.DEBUG)
-            MobclickAgent.onResume(mContext);
-    }
-
-    public void onPause() {
-        super.onPause();
-        if (!BuildConfig.DEBUG)
-            MobclickAgent.onPause(mContext);
-    }
-
 }
--- app/src/main/java/com/ovwvwvo/appinfos/ui/BaseFragment.java
+++ app/src/main/java/com/ovwvwvo/appinfos/ui/BaseFragment.java
@@ -3,9 +3,6 @@ package com.ovwvwvo.appinfos.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.appcompat.BuildConfig;
-
-import com.umeng.analytics.MobclickAgent;
 
 /**
  * Copyright ©2016 by rawer
@@ -18,17 +15,5 @@ public class BaseFragment extends com.ovwvwvo.common.fragment.BaseFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = getContext();
-    }
-
-    public void onResume() {
-        super.onResume();
-        if (!BuildConfig.DEBUG)
-            MobclickAgent.onResume(mContext);
-    }
-
-    public void onPause() {
-        super.onPause();
-        if (!BuildConfig.DEBUG)
-            MobclickAgent.onPause(mContext);
     }
 }
